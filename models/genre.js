18d19ff937f9c094ddbfb35a2a5ddee8bf1ba82d
Harshit Sai Kannam <kannam.h@northeastern.edu>
2024-03-18 17:54:38 -0400

    ip

--- models/genre.js
+++ models/genre.js
@@ -3,7 +3,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var GenreSchema = new Schema(
-  {}
+  {
+    name: { type: String, required: true, min: 3, max: 100 },
+    url: { type: String }
+  }
 );
 
 
